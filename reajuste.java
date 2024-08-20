48a69d7a93fca9351aaa3a71daf48d580d7b0805
teste7771 <vitormanoelalmeida2000@gmail.com>
2024-08-20 12:41:24 -0300

    4th lesson

--- reajuste.java
+++ reajuste.java
@@ -1,18 +1,22 @@
+
 //importaçoes
 import java.util.Scanner;
 
 public class reajuste {
 
 	public static void main(String[] args) {
-		Scanner leia = new Scanner (System.in);
-		float saldo, porcentagemSaldo;
+		Scanner leia = new Scanner(System.in);
+		float salarioMinimo = 1412, salario, qntdSalarioMinimo;
 
-		System.out.println("Informe o saldo: ");
-		saldo = leia.nextFloat();
-	
-		porcentagemSaldo = saldo + (saldo * 0.01f);
+		System.out.println("Informe o seu salario");
+		salario = leia.nextFloat();
 
-		System.out.println("Saldo final com reajuste: " +porcentagemSaldo);
-		
+		if (salario >= salarioMinimo) {
+			qntdSalarioMinimo = salario / salarioMinimo;
+			System.out.println("Você ganha " +(int)qntdSalarioMinimo + " salario(s) minimo(s)");
+		} else {
+			System.out.println("Você ganha menos de um salario minimo KKKKKKKKKKKKKKKKKKKKKKK");
+
+		}
 	}
 }
